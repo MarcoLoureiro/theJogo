6996730969d5e8aedc802e712d109be01c1acbd5
Kinho <marcoantonioloureiro098@gmail.com>
2022-12-16 14:50:25 -0300

    Criação do método para preencher o tabuleiro

--- JogoDaVelha/src/Jogo.java
+++ JogoDaVelha/src/Jogo.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Jogo {
     public static void main(String[] args) {
         char[][] tabuleiro = new char[3][3];
     }
+
+    public static void preencheTabuleiro(char[][] tabuleiro){
+
+        for (int i = 0; i < tabuleiro.length; i++) {
+            for (int j = 0; j < tabuleiro[0].length; j++) {
+                tabuleiro[i][j] = '_';
+                System.out.print(tabuleiro[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
 }
